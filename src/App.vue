96a2671d5d13de6aa75cab1a4015236f013caa6c
charles <cgiguere@iname.com>
2024-06-17 16:51:49 -0400

    app.vue in script setup

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,14 @@
         </v-row>
         <v-row>
           <v-col>
-            <v-select v-model="flashSize" :items="flashSizes" item-value="value" item-title="text" label="Flash Size"
+            <v-select v-model="flashSize" :items="FLASH_SIZES" item-value="value" item-title="text" label="Flash Size"
               dense hide-details></v-select>
           </v-col>
         </v-row>
         <v-row>
           <v-col>
-            <v-select v-model="selectedPartitionSet" :items="partitionOptions" item-value="value" item-title="text" label="Partition Set"
-              dense hide-details @change="loadPartitions"></v-select>
+            <v-select v-model="selectedPartitionSet" :items="partitionOptions" item-value="value" item-title="text"
+              label="Partition Set" dense hide-details @change="loadPartitions"></v-select>
           </v-col>
         </v-row>
         <v-row>
@@ -39,7 +39,7 @@
   </v-app>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent, ref, computed } from 'vue';
 import PartitionEditor from './components/PartitionEditor.vue';
 import PartitionVisualizer from './components/PartitionVisualizer.vue';
@@ -48,78 +48,59 @@ import { partitionStore } from '@/store';
 import type { Partition } from '@/types';
 import { esp32Partitions } from '@/partitions';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    PartitionEditor,
-    PartitionVisualizer
-  },
-  setup() {
-    const store = partitionStore();
+const store = partitionStore();
 
-    const flashSize = ref(4);
-    const partitions = ref<Partition[]>(esp32Partitions[0].partitions);
-    const selectedPartitionSet = ref(esp32Partitions[0].name);
+const flashSize = ref(4);
+const partitions = ref<Partition[]>(esp32Partitions[0].partitions);
+const selectedPartitionSet = ref(esp32Partitions[0].name);
 
-    const partitionOptions = esp32Partitions.map(set => ({
-      text: set.name,
-      value: set.name
-    }));
+const partitionOptions = esp32Partitions.map(set => ({
+  text: set.name,
+  value: set.name
+}));
 
-    const flashSizeBytes = computed(() => flashSize.value * 1024 * 1024);
+const flashSizeBytes = computed(() => flashSize.value * 1024 * 1024);
 
-    const updatePartitions = (newPartitions: Partition[]) => {
-      partitions.value = newPartitions;
-      const total = partitions.value.reduce((sum, partition) => sum + partition.size, 0);
-      const wastedMemory = calculateAlignmentWaste();
-      store.availableMemory = flashSizeBytes.value - PARTITION_TABLE_SIZE - total - wastedMemory;
-    };
+const updatePartitions = (newPartitions: Partition[]) => {
+  partitions.value = newPartitions;
+  const total = partitions.value.reduce((sum, partition) => sum + partition.size, 0);
+  const wastedMemory = calculateAlignmentWaste();
+  store.availableMemory = flashSizeBytes.value - PARTITION_TABLE_SIZE - total - wastedMemory;
+};
 
-    const calculateAlignmentWaste = () => {
-      let wastedSpace = 0;
-      partitions.value.forEach((partition, index) => {
-        if (index === 0) {
-          if (partition.type === 'app') {
-            const startOffset = 0x10000;
-            wastedSpace += partition.offset - startOffset;
-          } else {
-            const startOffset = 0x9000;
-            wastedSpace += partition.offset - startOffset;
-          }
-        } else {
-          let previousPartition = partitions.value[index - 1];
-          let previousOffsetEnd = previousPartition.offset + previousPartition.size;
-          if (partition.type === 'app') {
-            const alignedOffset = Math.ceil(previousOffsetEnd / 0x10000) * 0x10000;
-            wastedSpace += alignedOffset - previousOffsetEnd;
-          } else {
-            const alignedOffset = Math.ceil(previousOffsetEnd / 0x1000) * 0x1000;
-            wastedSpace += alignedOffset - previousOffsetEnd;
-          }
-        }
-      });
-      return wastedSpace;
-    };
-
-    const loadPartitions = () => {
-      const selectedSet = esp32Partitions.find(set => set.name === selectedPartitionSet.value);
-      if (selectedSet) {
-        partitions.value = [...selectedSet.partitions];
+const calculateAlignmentWaste = () => {
+  let wastedSpace = 0;
+  partitions.value.forEach((partition, index) => {
+    if (index === 0) {
+      if (partition.type === 'app') {
+        const startOffset = 0x10000;
+        wastedSpace += partition.offset - startOffset;
+      } else {
+        const startOffset = 0x9000;
+        wastedSpace += partition.offset - startOffset;
       }
-    };
+    } else {
+      let previousPartition = partitions.value[index - 1];
+      let previousOffsetEnd = previousPartition.offset + previousPartition.size;
+      if (partition.type === 'app') {
+        const alignedOffset = Math.ceil(previousOffsetEnd / 0x10000) * 0x10000;
+        wastedSpace += alignedOffset - previousOffsetEnd;
+      } else {
+        const alignedOffset = Math.ceil(previousOffsetEnd / 0x1000) * 0x1000;
+        wastedSpace += alignedOffset - previousOffsetEnd;
+      }
+    }
+  });
+  return wastedSpace;
+};
 
-    return {
-      flashSizes: FLASH_SIZES,
-      flashSize,
-      partitions,
-      updatePartitions,
-      flashSizeBytes,
-      partitionOptions,
-      selectedPartitionSet,
-      loadPartitions
-    };
+const loadPartitions = () => {
+  const selectedSet = esp32Partitions.find(set => set.name === selectedPartitionSet.value);
+  if (selectedSet) {
+    partitions.value = [...selectedSet.partitions];
   }
-});
+};
+
 </script>
 
 <style scoped></style>
