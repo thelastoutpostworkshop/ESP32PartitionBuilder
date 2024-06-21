b37fc1e825ceb65d00cb6ccba7d61c091e696dad
charles <cgiguere@iname.com>
2024-06-21 07:39:47 -0400

    esp32Partitions

--- src/defaultPartitions.ts
+++ src/defaultPartitions.ts
@@ -2,20 +2,26 @@ import type { PartitionSet } from '@/types';
 
 export const esp32Partitions: PartitionSet[] = [
     {
-      name: 'Partition Set 1',
+      name: '4MB with spiffs (1.2MP APP/1.5MB SPIFFS)',
       partitions: [
-        { name: 'nvs', type: 'data', subtype: 'nvs', size: 16, offset: 0, flags: "" },
+        { name: 'nvs', type: 'data', subtype: 'nvs', size: 20, offset: 0, flags: "" },
         { name: 'otadata', type: 'data', subtype: 'ota', size: 8, offset: 0, flags: "" },
         { name: 'app0', type: 'app', subtype: 'ota_0', size: 1280, offset: 0, flags: "" },
         { name: 'app1', type: 'app', subtype: 'ota_1', size: 1280, offset: 0, flags: "" },
+        { name: 'spiffs', type: 'app', subtype: 'spiffs', size: 1408, offset: 0, flags: "" },
+        { name: 'coredump', type: 'data', subtype: 'coredump', size: 64, offset: 0, flags: "" },
       ]
     },
     {
-      name: 'Partition Set 2',
-      partitions: [
-        { name: 'storage', type: 'data', subtype: 'spiffs', size: 1024, offset: 0, flags: "" },
-        { name: 'app2', type: 'app', subtype: 'factory', size: 2048, offset: 0, flags: "" },
-      ]
-    }
+        name: '4MB with fat (1.2MP APP/1.5MB FAT)',
+        partitions: [
+          { name: 'nvs', type: 'data', subtype: 'nvs', size: 20, offset: 0, flags: "" },
+          { name: 'otadata', type: 'data', subtype: 'ota', size: 8, offset: 0, flags: "" },
+          { name: 'app0', type: 'app', subtype: 'ota_0', size: 1280, offset: 0, flags: "" },
+          { name: 'app1', type: 'app', subtype: 'ota_1', size: 1280, offset: 0, flags: "" },
+          { name: 'spiffs', type: 'app', subtype: 'spiffs', size: 1408, offset: 0, flags: "" },
+          { name: 'coredump', type: 'data', subtype: 'coredump', size: 64, offset: 0, flags: "" },
+        ]
+      },
   ];
   
