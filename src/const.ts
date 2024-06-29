570c2eea4c4c579f96c9d27253959dfa045876d1
charles <cgiguere@iname.com>
2024-06-29 13:02:29 -0400

    OTA_DATA_PARTITION_SIZE

--- src/const.ts
+++ src/const.ts
@@ -3,6 +3,7 @@ export const OFFSET_APP_TYPE = 0x10000
 export const OFFSET_DATA_TYPE = 0x1000
 export const PARTITION_TABLE_SIZE = 0x9000; // 36KB reserved for the partition table
 export const NVS_PARTITION_SIZE_RECOMMENDED = 0x3000
+export const OTA_DATA_PARTITION_SIZE = 0x2000
 
 export const FLASH_SIZES = [
     { value: 4, text: '4 MB' },
