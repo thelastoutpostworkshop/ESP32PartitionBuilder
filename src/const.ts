8fde57935ba9110950e6e56d81a35b922a29ad57
Charles <cgiguere@iname.com>
2024-07-01 11:45:00 -0400

    PARTITION_PHY

--- src/const.ts
+++ src/const.ts
@@ -8,6 +8,7 @@ export const FAT_MIN_PARTITION_SIZE = 528 * 1024
 export const SPIFFS_MIN_PARTITION_SIZE = 192 * 1024
 export const LITTLEFS_MIN_PARTITION_SIZE = 128 * 1024
 export const COREDUMP_MIN_PARTITION_SIZE = 64 * 1024
+export const PHY_MIN_PARTITION_SIZE = 4 * 1024
 
 export const FLASH_SIZES = [
     { value: 4, text: '4 MB' },
@@ -24,6 +25,7 @@ export const PARTITION_LITTLEFS = "littlefs"
 export const PARTITION_COREDUMP = "coredump"
 export const PARTITION_FACTORY = "factory"
 export const PARTITION_TEST = "test"
+export const PARTITION_PHY = "phy"
 export const PARTITION_TYPES = [PARTITION_TYPE_APP, PARTITION_TYPE_DATA]
 export const PARTITION_APP_SUBTYPES = [PARTITION_FACTORY, PARTITION_TEST, 'ota_0', 'ota_1', 'ota_2', 'ota_3', 'ota_4', 'ota_5', 'ota_6', 'ota_7', 'ota_8', 'ota_9', 'ota_10', 'ota_11', 'ota_12', 'ota_13', 'ota_14', 'ota_15']
-export const PARTITION_DATA_SUBTYPES = [PARTITION_OTA, 'phy', PARTITION_NVS, 'nvs_keys', PARTITION_COREDUMP, 'efuse', PARTITION_FAT, PARTITION_SPIFFS, PARTITION_LITTLEFS]
+export const PARTITION_DATA_SUBTYPES = [PARTITION_OTA, PARTITION_PHY, PARTITION_NVS, 'nvs_keys', PARTITION_COREDUMP, 'efuse', PARTITION_FAT, PARTITION_SPIFFS, PARTITION_LITTLEFS]
