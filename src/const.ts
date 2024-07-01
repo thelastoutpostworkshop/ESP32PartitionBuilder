34ee6977dbf6d0d1540ed8bd6063f3aef86c3b4b
Charles <cgiguere@iname.com>
2024-07-01 12:59:05 -0400

    FLASH_SIZES 32MB

--- src/const.ts
+++ src/const.ts
@@ -13,7 +13,8 @@ export const PHY_MIN_PARTITION_SIZE = 4 * 1024
 export const FLASH_SIZES = [
     { value: 4, text: '4 MB' },
     { value: 8, text: '8 MB' },
-    { value: 16, text: '16 MB' }
+    { value: 16, text: '16 MB' },
+    { value: 32, text: '32 MB' }
 ];
 export const PARTITION_TYPE_APP = "app"
 export const PARTITION_TYPE_DATA = "data"
