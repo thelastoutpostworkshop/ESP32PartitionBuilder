b2931902cdb24c45b36b74099c404588ad575f1d
charles <cgiguere@iname.com>
2024-07-02 18:55:58 -0400

    esp32Partitions

--- src/defaultPartitions.ts
+++ src/defaultPartitions.ts
@@ -12,7 +12,7 @@ export const esp32Partitions: PartitionSet[] = [
       { name: 'otadata', type: 'data', subtype: 'ota', size: 0x2000, offset: 0, flags: "" },
       { name: 'app0', type: 'app', subtype: 'ota_0', size: 0x140000, offset: 0, flags: "" },
       { name: 'app1', type: 'app', subtype: 'ota_1', size: 0x140000, offset: 0, flags: "" },
-      { name: 'spiffs', type: 'app', subtype: 'spiffs', size: 0x160000, offset: 0, flags: "" },
+      { name: 'spiffs', type: 'data', subtype: 'spiffs', size: 0x160000, offset: 0, flags: "" },
       { name: 'coredump', type: 'data', subtype: 'coredump', size: 0x10000, offset: 0, flags: "" },
     ]
   },
@@ -23,7 +23,7 @@ export const esp32Partitions: PartitionSet[] = [
       { name: 'otadata', type: 'data', subtype: 'ota', size: 0x2000, offset: 0, flags: "" },
       { name: 'app0', type: 'app', subtype: 'ota_0', size: 0x140000, offset: 0, flags: "" },
       { name: 'app1', type: 'app', subtype: 'ota_1', size: 0x140000, offset: 0, flags: "" },
-      { name: 'fat', type: 'app', subtype: 'fat', size: 0x160000, offset: 0, flags: "" },
+      { name: 'fat', type: 'data', subtype: 'fat', size: 0x160000, offset: 0, flags: "" },
       { name: 'coredump', type: 'data', subtype: 'coredump', size: 0x10000, offset: 0, flags: "" },
     ]
   },
