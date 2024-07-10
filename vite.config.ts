01297e5b0909bfff73444b22b74d8f5c66f3e34a
charles <cgiguere@iname.com>
2024-07-10 06:36:52 -0400

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
+  base: "/microcontroller_devkit/ESP32PartitionBuilder/"
 })
