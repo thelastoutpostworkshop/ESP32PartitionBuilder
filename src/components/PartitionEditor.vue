2be3ac42523b28b8c9b8c756a304836690638c23
serek4 <jacezarem@gmail.com>
2025-03-24 21:00:21 +0100

    make `flags` optional in header line in csv file, ignore more than one `#` in header line in csv file

--- src/components/PartitionEditor.vue
+++ src/components/PartitionEditor.vue
@@ -557,10 +557,10 @@ const handleFileUpload = (event: Event) => {
 };
 
 const loadPartitionsFromCSV = (csv: string) => {
-  const validHeader = '#Name,Type,SubType,Offset,Size,Flags';
-  const rows = csv.replaceAll(/[ \t\r]+/g, '').split('\n').filter(row => row === validHeader || (row !== '' && !row.startsWith('#')));
-  const header = rows.shift(); // Remove the header row
-  if (header !== validHeader || rows.length === 0) {
+  const validHeader = /#+Name,Type,SubType,Offset,Size(,Flags)?/;
+  const rows = csv.replaceAll(/[ \t\r]+/g, '').split('\n').filter(row => validHeader.test(row) || (row !== '' && !row.startsWith('#')));
+  const header = rows.shift() || ''; // Remove the header row
+  if (!validHeader.test(header) || rows.length === 0) {
     alertTitle.value = "Invalid CSV Format";
     alertText.value = "The CSV file format is incorrect. Please use the correct format.";
     showAlert.value = true;
