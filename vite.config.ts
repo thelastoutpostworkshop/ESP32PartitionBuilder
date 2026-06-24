c752764f63e64ccc9f6db782757cdcb542b4a682
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2026-06-24 18:21:38 -0400

    source app version from package metadata

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import packageJson from './package.json' with { type: 'json' }
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
